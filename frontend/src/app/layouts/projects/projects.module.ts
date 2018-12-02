651633fb5bb72bd6958e2b7495eb513713cd6ef9
Diana <dl11021997@gmail.com>
2018-12-02 16:55:10 +0200

    highlight

--- frontend/src/app/layouts/projects/projects.module.ts
+++ frontend/src/app/layouts/projects/projects.module.ts
@@ -12,19 +12,7 @@ import {FormsModule} from '@angular/forms';
 import {SafePipe} from './safe.pipe';
 import {HighlightModule} from 'ngx-highlightjs';
 
-import xml from 'highlight.js/lib/languages/xml';
-import scss from 'highlight.js/lib/languages/scss';
-import typescript from 'highlight.js/lib/languages/typescript';
-import javascript from 'highlight.js/lib/languages/javascript';
-
-function HLLanguages() {
-    return [
-        {name: 'typescript', func: typescript},
-        {name: 'javascript', func: javascript},
-        {name: 'scss', func: scss},
-        {name: 'xml', func: xml}
-    ];
-}
+import {HLLanguages} from '../../app.module';
 
 @NgModule({
     imports: [
