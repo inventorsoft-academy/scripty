698a81b0726702328c4eac71b58287967a3b62ea
Diana <dl11021997@gmail.com>
2018-12-13 00:09:16 +0200

    made highlight coda and added data to localStorage

--- frontend/src/app/layouts/projects/project/project.component.ts
+++ frontend/src/app/layouts/projects/project/project.component.ts
@@ -1,5 +1,4 @@
 import {Component, OnInit} from '@angular/core';
-import {HighlightResult} from 'ngx-highlightjs';
 
 
 @Component({
@@ -12,68 +11,28 @@ export class ProjectComponent implements OnInit {
     html: string;
     style: string;
     script: string;
-    htmlCode: string;
-    styleCode: string;
-    scriptCode: string;
 
     public constructor() {
-        this.html = '<div>asfaf</div>';
-        this.script = '';
-        this.style = '';
+        this.html = localStorage.getItem('html') || '';
+        this.script = localStorage.getItem('script') || '';
+        this.style = localStorage.getItem('style') || '';
         this.run();
-
     }
 
     ngOnInit() {
     }
 
-    onInputCode(e: any, type: string) {
-        console.log(e, type, e.target.value);
-        if (this.hasOwnProperty(type)) {
-            console.log(e.target.selectionStart, e.target.selectionEnd);
-            const cursor = '▏',
-                value = e.target.value.replace(new RegExp(cursor, 'g'), '');
-            let cursorPosition = 0;
-            if (e.target.selectionStart === e.target.selectionEnd) {
-                cursorPosition = e.target.selectionStart;
-            }
-            this[type] = value;
-            // this[`${type}Code`] = `${value.substring(0, cursorPosition)}${cursor}${value.substring(cursorPosition)}`;
-            this[`${type}Code`] = `${value.substring(0, cursorPosition)}${value.substring(cursorPosition)}`;
-        }
-    }
-    onMouseDown(e: any, type: string) {
-        console.log(e, type, e.target.value);
-        if (this.hasOwnProperty(type)) {
-            console.log(e.target.selectionStart, e.target.selectionEnd);
-            const cursor = '▏',
-                value = this.html.replace(new RegExp(cursor, 'g'), '');
-            let cursorPosition = 0;
-            if (e.target.selectionStart === e.target.selectionEnd) {
-                cursorPosition = e.target.selectionStart;
-            }
-            this[type] = value;
-            // this[`${type}Code`] = `${value.substring(0, cursorPosition)}${cursor}${value.substring(cursorPosition)}`;
-            this[`${type}Code`] = `${value.substring(0, cursorPosition)}${value.substring(cursorPosition)}`;
-        }
-    }
-
-
     onBlurCode(type: string) {
-        if (this.hasOwnProperty(`${type}Code`)) {
-            this[`${type}Code`] = this[type];
-        }
+        localStorage.setItem(type, this[type]);
     }
 
-
-    onHighlight(e: any) {
-        console.log(e, this.htmlCode);
+    onInputCode(e: any, type: string) {
+        if (this.hasOwnProperty(type)) {
+            this[type] = e.target.value;
+        }
     }
 
     run() {
-        this.htmlCode = this.html;
-        this.styleCode = this.style;
-        this.scriptCode = this.script;
         this.result = `data:text/html;charset = utf - 8,<html>
                 <head>
                     <style>${this.style}</style>
