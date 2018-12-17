52873add869632f1514112a74b806e3539e7b41a
Vadim <vkravchik@gmail.com>
2018-12-17 13:55:29 +0200

    cleane reports components

--- frontend/src/app/layouts/admin-page/reports/report.component.ts
+++ frontend/src/app/layouts/admin-page/reports/report.component.ts
@@ -1,6 +1,4 @@
 import {Component, HostListener, OnInit, ViewChild} from '@angular/core';
-import {CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
-import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-report',
@@ -10,58 +8,7 @@ import {HttpClient, HttpClientModule} from "@angular/common/http";
 
 export class ReportComponent implements OnInit {
 
-    users = [
-        ['User', 'Registered Users'],
-    ];
-
-    userChartData = {
-        chartType: 'LineChart',
-        dataTable: this.users,
-        options: {'title': 'Users'},
-        chartArea: {left: 0, top: 0}
-    };
-
-    projectChartData = {
-        chartType: 'LineChart',
-        dataTable: [
-            ['Project', 'Created Project'],
-            ['28.07.2018', 3],
-            ['01.08.2018', 5],
-            ['02.08.2018', 2],
-            ['02.08.2018', 19],
-            ['05.08.2018', 24]
-        ],
-        options: {'title': 'Projects'},
-        chartArea: {left: 0, top: 0}
-    };
-
-    otherChartData = {
-        chartType: 'LineChart',
-        dataTable: [
-            ['Project', 'Created Project'],
-            ['28.07.2018', 6],
-            ['01.08.2018', 1],
-            ['02.08.2018', 19],
-            ['02.08.2018', 4],
-            ['05.08.2018', 88],
-            ['06.08.2018', 88],
-            ['07.08.2018', 89],
-        ],
-        options: {'title': 'Projects', 'width': '100%'},
-        chartArea: {left: 0, top: 0}
-    };
-
-    arrayOfCharts = [
-        // this.userChartData,
-        this.projectChartData,
-        this.otherChartData,
-    ];
-
-  constructor(private http: HttpClient) { }
-
-    drop($event: CdkDragDrop<String[]>) {
-        moveItemInArray(this.arrayOfCharts, $event.currentIndex, $event.previousIndex);
-    }
+  constructor() { }
 
   ngOnInit() {
   }
