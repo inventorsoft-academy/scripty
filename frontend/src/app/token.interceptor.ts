260fcdbbe9aee275e8110db54509f215ec2310bf
Kudesnitsa <dl11021997@gmail.com>
2018-12-28 13:18:16 +0200

    fix mock

--- frontend/src/app/token.interceptor.ts
+++ frontend/src/app/token.interceptor.ts
@@ -35,12 +35,12 @@ export class HttpTokenInterceptor implements HttpInterceptor {
             // todo for developer
             /* tslint:disable */
             tokenData = {
-                "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsiand0cmVzb3VyY2VpZCJdLCJ1c2VyX25hbWUiOiJ1c2VyQHRlc3QuY28iLCJzY29wZSI6WyJyZWFkIiwid3JpdGUiXSwiZXhwIjoxNTQ1OTkyNDYzLCJhdXRob3JpdGllcyI6WyJST0xFX1VTRVIiXSwianRpIjoiNzExMWNhNGYtODliMy00ZDVhLTk0ZjEtZTEzMWRmNTM3NWFlIiwiY2xpZW50X2lkIjoiand0Y2xpZW50aWQifQ.-MnIuz_G0JQXLrTjS5T7JvtLAJ4e_UjvO8AvL_lHAsU",
+                "access_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsiand0cmVzb3VyY2VpZCJdLCJ1c2VyX25hbWUiOiJ1c2VyQHRlc3QuY28iLCJzY29wZSI6WyJyZWFkIiwid3JpdGUiXSwiZXhwIjoxNTQ1OTk2NDkwLCJhdXRob3JpdGllcyI6WyJST0xFX1VTRVIiXSwianRpIjoiNjQ1ZWU0MDktOTc0ZC00Yjc5LWJhN2YtYzk0NzQ4MWZkM2FkIiwiY2xpZW50X2lkIjoiand0Y2xpZW50aWQifQ.BNOVOiPD9VFamdMVCf7tbW3JKLl-5-mKMeowf8Vg8ug",
                 "token_type": "bearer",
-                "refresh_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsiand0cmVzb3VyY2VpZCJdLCJ1c2VyX25hbWUiOiJ1c2VyQHRlc3QuY28iLCJzY29wZSI6WyJyZWFkIiwid3JpdGUiXSwiYXRpIjoiNzExMWNhNGYtODliMy00ZDVhLTk0ZjEtZTEzMWRmNTM3NWFlIiwiZXhwIjoxNTQ2MDA1OTYzLCJhdXRob3JpdGllcyI6WyJST0xFX1VTRVIiXSwianRpIjoiYjA2MzBlZWEtN2QyYS00NDY2LWE2YWItYjZkOTJkYzZhNTViIiwiY2xpZW50X2lkIjoiand0Y2xpZW50aWQifQ.IdosxiO6CEa1HfmWEpcST5JfaEmOMHeM0ZL5NanmShA",
+                "refresh_token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOlsiand0cmVzb3VyY2VpZCJdLCJ1c2VyX25hbWUiOiJ1c2VyQHRlc3QuY28iLCJzY29wZSI6WyJyZWFkIiwid3JpdGUiXSwiYXRpIjoiNjQ1ZWU0MDktOTc0ZC00Yjc5LWJhN2YtYzk0NzQ4MWZkM2FkIiwiZXhwIjoxNTQ2MDA5OTkwLCJhdXRob3JpdGllcyI6WyJST0xFX1VTRVIiXSwianRpIjoiYmM2ZWFjYzUtNjZlOC00NThmLTg1YmItNmI5MjdkMTVjYjVhIiwiY2xpZW50X2lkIjoiand0Y2xpZW50aWQifQ.Q3TLv3EbYFpmN82O8LVeTyO_CEZTggqwAx_zMwVLItM",
                 "expires_in": 899,
                 "scope": "read write",
-                "jti": "7111ca4f-89b3-4d5a-94f1-e131df5375ae"
+                "jti": "645ee409-974d-4b79-ba7f-c947481fd3ad"
             };
             TokenService.setTokenData(
                 tokenData
