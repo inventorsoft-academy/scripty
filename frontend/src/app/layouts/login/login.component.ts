5fc206af3e7c827f124ad229e0e193fda41a61e2
Oleh Chepak <o.chepak@outlook.com>
2019-01-01 20:37:58 +0200

    merged with project-list

--- frontend/src/app/layouts/login/login.component.ts
+++ frontend/src/app/layouts/login/login.component.ts
@@ -1,30 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 import {AuthService} from '../../services/auth.service';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
     loginForm: FormGroup;
     emailError: string;
 
 
-  constructor(
-      private _formBuilder: FormBuilder,
-      private router: Router,
-      private _auth: AuthService) { }
+    constructor(
+        private _formBuilder: FormBuilder,
+        private router: Router,
+        private _auth: AuthService) {
+    }
 
-  ngOnInit() {
-      this.loginForm = this._formBuilder.group({
-          username: ['', [Validators.required, Validators.email]],
-          password: ['', Validators.required],
-          grant_type: 'password'
-      });
-  }
+    ngOnInit() {
+        this.loginForm = this._formBuilder.group({
+            username: ['', [Validators.required, Validators.email]],
+            password: ['', Validators.required],
+            grant_type: 'password'
+        });
+    }
+
     emailValidator() {
         const _field = this.loginForm.get('username');
         if (_field.hasError('required') && _field.touched) {
@@ -36,15 +38,16 @@ export class LoginComponent implements OnInit {
         }
         return false;
     }
+
     authUser() {
         this._auth.authUser(this.loginForm.value)
             .subscribe(
                 res => {
-                  localStorage.setItem('access_token', res.access_token);
-                  localStorage.setItem('refresh_token', res.refresh_token);
-                  localStorage.setItem('user', this.loginForm.value.username);
-                  this.router.navigate(['/projects']);
-                  console.log(localStorage.getItem('user'));
+                    localStorage.setItem('access_token', res.access_token);
+                    localStorage.setItem('refresh_token', res.refresh_token);
+                    localStorage.setItem('user', this.loginForm.value.username);
+                    this.router.navigate(['/projects']);
+                    console.log(localStorage.getItem('user'));
                 },
                 err => {
                     alert(err.error.error_description);
