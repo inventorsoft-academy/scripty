be192938a628759efa9d2845e0e3ff6645afb981
Leonid Zabidovsky <leonid.zabidovsky@gmail.com>
2019-01-05 16:53:15 +0200

    ticket-picture

--- backend/src/main/java/co/inventorsoft/scripty/controller/TicketController.java
+++ backend/src/main/java/co/inventorsoft/scripty/controller/TicketController.java
@@ -1,7 +1,6 @@
 package co.inventorsoft.scripty.controller;
 
 import java.util.List;
-
 import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,10 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import co.inventorsoft.scripty.model.dto.PictureDto;
 import co.inventorsoft.scripty.model.dto.StringResponse;
 import co.inventorsoft.scripty.model.dto.TicketDto;
 import co.inventorsoft.scripty.model.dto.TicketToFront;
@@ -39,9 +40,9 @@ public class TicketController {
 
 	@ApiOperation(value = "Endpoint to create new ticket. The endpoint consumes fields: title(required), description(required).")
 	@PreAuthorize("hasAuthority('ROLE_USER')")
-	@PostMapping(consumes = "application/json")
-	public ResponseEntity<StringResponse> saveTicket(Authentication auth, @Valid @RequestBody TicketDto ticket) {
-		long ticketId = ticketService.saveTicket(ticket, auth.getName());
+	@PostMapping(consumes = "multipart/form-data")
+	public ResponseEntity<StringResponse> saveTicket(Authentication auth, @Valid @RequestPart TicketDto ticket, @RequestPart(required=false) MultipartFile picture) {
+		long ticketId = ticketService.saveTicket(ticket, auth.getName(), picture);
 		return ResponseEntity.status(HttpStatus.CREATED).body(new StringResponse("New ticket was created with ID = " + ticketId));
 	}
 
@@ -58,6 +59,12 @@ public class TicketController {
 	public ResponseEntity<StringResponse> archiveTicket(@PathVariable Long ticketId, @RequestParam boolean archive) {
 		ticketService.archiveTicket(ticketId, archive);
 		return ResponseEntity.ok(new StringResponse("Ticket ID = " + ticketId + " archive status was changed"));
+	}
+
+	@ApiOperation(value = "Endpoint to get picture for ticket.")
+	@GetMapping(value = "/{ticketId}/picture", produces = "application/json")
+	public ResponseEntity<PictureDto> getPicture(@PathVariable Long ticketId) {
+		return ResponseEntity.ok(ticketService.getPicture(ticketId));
 	}
 
 }
